3da270c2401951f4156d98f1ee25df9a8536dcc0
franferri <franferri@gmail.com>
2020-01-24 01:05:53 +0000

    Updates all tutorials

--- 4. Test your components individually/LCD_16x2_I2C_Screen/screen_tests.py
+++ 4. Test your components individually/LCD_16x2_I2C_Screen/screen_tests.py
@@ -1,7 +1,7 @@
 import RPi_LCD_16x2_I2C_driver
 from time import *
 
-mylcd = RPi_I2C_driver.lcd()
+mylcd = RPi_LCD_16x2_I2C_driver.lcd()
 # test 2
 mylcd.lcd_display_string("RPi I2C test", 1)
 mylcd.lcd_display_string(" Custom chars", 2)
